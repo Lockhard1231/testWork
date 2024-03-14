1a6642cd81100f03fe243bc663ea99191e6a39a8
Khasanov Rinat <leo1231914@mail.ru>
2024-03-14 15:02:24 +0500

    Изменение обработки коротких URL, а так же добавлен редирект.

--- ajax-request.js
+++ ajax-request.js
@@ -6,30 +6,10 @@ function generateShortUrl() {
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                document.getElementById("shortUrl").innerHTML = "<a href='" + xhr.responseText + "' id = 'shortUrlLink' class='myLink'>" + xhr.responseText + "</a>";
+                document.getElementById("shortUrl").innerHTML = "<a href='" + xhr.responseText + "'>" + xhr.responseText + "</a>";
             }
         }
     };
     xhr.send("originalUrl=" + encodeURIComponent(originalUrl));
 }
 
-function redirectionURL(shortUrl) {
-    console.log('Redirecting to:', shortUrl);
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", "redirect.php?shortUrl=" + encodeURIComponent(shortUrl), true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    xhr.onreadystatechange = function () {
-        console.log('Redirecting response code:', xhr.status);
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var response = JSON.parse(xhr.responseText);
-                if (response.url) {
-                    // Use window.open to open the URL in a new window or tab
-                    window.open(response.url, '_blank');
-                } 
-            }
-        }
-    };
-
-    xhr.send(null);
-}
--- redirect.php
+++ redirect.php
@@ -1,7 +1,7 @@
 <?php
 require 'db_connect.php';
 
-$shortUrl = $_GET['shortUrl'] ?? '';
+$shortUrl = $_GET['short_url'] ?? '';
 
 
 // $query = $connect->prepare("SELECT original_url, validity FROM urls WHERE short_url = ? AND validity > NOW()");
@@ -15,12 +15,13 @@ $row = $query->fetch(PDO::FETCH_ASSOC);
 
 if ($row) {
     // Instead of redirecting, send back a JSON response
-    echo json_encode(['url' => $row['original_url']]);
-    exit;
+    header("Location: " . $row['original_url']);
+    exit();
 } else {
     //Если короткая ссылка не найдена или устарела, перенаправляем на форму ввода новой ссылки
     // Instead of redirecting, send back a JSON response
-    echo json_encode(['url' => 'index.html']);
-    exit;
+    header("Location: " . 'index.php');
+    //echo json_encode(['url' => 'index.html']);
+    exit();
 }
 ?>
